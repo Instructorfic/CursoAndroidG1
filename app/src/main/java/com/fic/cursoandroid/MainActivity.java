0bd5377411b9a291def0f1d37389b0323b7d7cd2
Alexis Montaño <alexis.montano@info.uas.edu.mx>
2021-10-20 17:54:17 -0600

    Confirmación Inicial

---
+++ app/src/main/java/com/fic/cursoandroid/MainActivity.java
@@ -0,0 +1,18 @@
+package com.fic.cursoandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_linear_layout);
+    }
+}
