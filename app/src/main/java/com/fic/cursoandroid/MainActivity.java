461e59b0a4921015c41d3cf1df94384b05e26b04
Alexis Montaño <alexis.montano@info.uas.edu.mx>
2021-11-08 11:02:15 -0700

    Sesión 08-11-2021

--- app/src/main/java/com/fic/cursoandroid/MainActivity.java
+++ app/src/main/java/com/fic/cursoandroid/MainActivity.java
@@ -2,7 +2,9 @@ package com.fic.cursoandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -11,8 +13,17 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_scrollview);
+        setContentView(R.layout.activity_constraint_layout);
+
+        Button btnLogin = (Button) findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent showActivity = new Intent(getApplicationContext(),MainActivity2.class);
+                startActivity(showActivity);
+            }
+        });
     }
 }
--- app/src/main/java/com/fic/cursoandroid/MainActivity2.java
+++ app/src/main/java/com/fic/cursoandroid/MainActivity2.java
@@ -2,7 +2,11 @@ package com.fic.cursoandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -10,5 +14,26 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        Button btnShowWebPage = (Button) findViewById(R.id.btnShowView);
+        btnShowWebPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("http://www.google.com");
+                Intent showWebPageIntent = new Intent(Intent.ACTION_VIEW,uri);
+                startActivity(showWebPageIntent);
+            }
+        });
+
+
+        Button btnDial = (Button) findViewById(R.id.btnDial);
+        btnDial.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri dialUri = Uri.parse("tel:6677000000");
+                Intent dialIntent = new Intent(Intent.ACTION_DIAL,dialUri);
+                startActivity(dialIntent);
+            }
+        });
     }
 }
