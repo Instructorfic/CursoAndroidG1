642889906c15bfb7374f75fd88202920784f251c
Alexis Montaño <alexis.montano@info.uas.edu.mx>
2021-11-19 10:55:49 -0700

    Sesión 19-11-2021

--- app/src/main/java/com/fic/cursoandroid/MainActivity.java
+++ app/src/main/java/com/fic/cursoandroid/MainActivity.java
@@ -5,18 +5,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
 
     final String webPageUrl = "http://info.uas.edu.mx";
+    final String MAIN_ACTIVITY_TAG = MainActivity.class.getSimpleName();
+
+    private void setLog(String text){
+        Log.d(MAIN_ACTIVITY_TAG,text);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setLog("onCreate Called");
         setContentView(R.layout.activity_constraint_layout);
 
         Button btnLogin = (Button) findViewById(R.id.btnLogin);
@@ -41,4 +49,41 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    protected void onStart(){
+        super.onStart();
+        setLog("onStart Called");
+        //Toast.makeText(getApplicationContext(), "onStart()", Toast.LENGTH_LONG).show();
+    }
+
+    protected void onResume(){
+        super.onResume();
+        setLog("onResume Called");
+        //Toast.makeText(getApplicationContext(), "onResume()", Toast.LENGTH_LONG).show();
+    }
+
+    protected void onPause(){
+        super.onPause();
+        setLog("onPause Called");
+        //Toast.makeText(getApplicationContext(), "onPause()", Toast.LENGTH_LONG).show();
+    }
+
+    protected void onRestart(){
+        super.onRestart();
+        setLog("onRestart Called");
+    }
+
+    protected void onStop(){
+        super.onStop();
+        setLog("onStop Called");
+
+    }
+
+    protected void onDestroy(){
+        super.onDestroy();
+        setLog("onDestroy Called");
+
+    }
+
+
 }
