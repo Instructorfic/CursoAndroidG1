ca86bf9fafb84e6bb6e0cdd169b17e1af8930298
Alexis Montaño <alexis.montano@info.uas.edu.mx>
2021-11-29 15:02:57 -0700

    Sesión 29-11-2021

--- app/src/main/java/com/fic/cursoandroid/MainActivity2.java
+++ app/src/main/java/com/fic/cursoandroid/MainActivity2.java
@@ -6,11 +6,14 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.PopupMenu;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -18,6 +21,7 @@ import org.w3c.dom.Text;
 
 public class MainActivity2 extends AppCompatActivity {
 
+    private ImageButton btnPopUp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,6 +43,28 @@ public class MainActivity2 extends AppCompatActivity {
         TextView txtMessage = (TextView) findViewById(R.id.txtMessage);
         TextView txtError = (TextView) findViewById(R.id.txtError);
         TextView txtName = findViewById(R.id.txtName);
+
+        /*Menú para contexto*/
+        TextView txtMenuContext = findViewById(R.id.txtMenuContext);
+        registerForContextMenu(txtMenuContext);
+
+        btnPopUp = findViewById(R.id.btnPopUp);
+
+        btnPopUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PopupMenu popup = new PopupMenu(MainActivity2.this,btnPopUp);
+                popup.getMenuInflater().inflate(R.menu.menu_popup, popup.getMenu());
+
+                popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        return false;
+                    }
+                });
+                popup.show();
+            }
+        });
 
         txtMessage.setText(message);
         txtError.setText(String.valueOf(error));
@@ -108,6 +134,25 @@ public class MainActivity2 extends AppCompatActivity {
             default:
                 return super.onOptionsItemSelected(item);
 
+        }
+    }
+
+    public void onCreateContextMenu(ContextMenu menu,View v, ContextMenu.ContextMenuInfo menuInfo){
+        super.onCreateContextMenu(menu,v,menuInfo);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_context,menu);
+    }
+
+    public boolean onContextItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.mnuContextEdit:
+                Toast.makeText(getApplicationContext(),"Editar",Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.mnuContextShare:
+                Toast.makeText(getApplicationContext(),"Compartir",Toast.LENGTH_LONG).show();
+                return true;
+            default :
+                    return super.onContextItemSelected(item);
         }
     }
 }
