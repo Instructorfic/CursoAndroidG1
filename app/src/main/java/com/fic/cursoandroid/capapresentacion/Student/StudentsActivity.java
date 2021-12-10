40e80c96abe99e2ae5d20f12bb4c3843e34563dd
Alexis Montaño <alexis.montano@info.uas.edu.mx>
2021-12-10 10:59:35 -0700

    Sesión 10-12-2021

--- app/src/main/java/com/fic/cursoandroid/capapresentacion/Student/StudentsActivity.java
+++ app/src/main/java/com/fic/cursoandroid/capapresentacion/Student/StudentsActivity.java
@@ -1,4 +1,34 @@
 package com.fic.cursoandroid.capapresentacion.Student;
 
-public class StudentsActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.fic.cursoandroid.R;
+import com.fic.cursoandroid.capapresentacion.Main.MainActivity;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class StudentsActivity extends AppCompatActivity {
+
+    private FloatingActionButton fabAddStudent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_students);
+
+        fabAddStudent = findViewById(R.id.fabAddStudent);
+        fabAddStudent.setOnClickListener(addStudentListener);
+    }
+
+    private View.OnClickListener addStudentListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(getApplicationContext(), NewStudentAcitivty.class);
+            startActivity(intent);
+        }
+    };
 }
